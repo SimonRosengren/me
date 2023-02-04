2a95cff121dd089fb32671763bea4325108bbe58
simonrosengren <simonrosengren123@gmail.com>
2023-02-04 23:16:14 +0100

    Let there be light

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      './components/**/*.{js,vue,ts}',
+      './layouts/**/*.vue',
+      './pages/**/*.vue',
+      './plugins/**/*.{js,ts}',
+      './nuxt.config.{js,ts}',
+    ],
+    theme: {
+      screens: {
+        xs: '300px',
+        sm: '768px',
+        md: '920px',
+        lg: '1330px'
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif']
+      },
+      extend: {
+        colors: {
+          main: {
+            100: '#0f0f0f'
+          },
+          fontFamily: {
+            'poppins': ['Poppins', 'sans-serif']
+          },
+          secondary: {
+            100: '#d9d9d9'
+          }
+        },
+      },
+    }
+  };

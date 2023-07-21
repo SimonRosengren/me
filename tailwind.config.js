58c417fafda406c829514fc30e378dfa3d1a1026
Simon <simon@tengai.io>
2023-07-21 23:06:32 +0200

    Bauhaus

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,46 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      './components/**/*.{js,vue,ts}',
-      './layouts/**/*.vue',
-      './pages/**/*.vue',
-      './plugins/**/*.{js,ts}',
-      './nuxt.config.{js,ts}',
-    ],
-    theme: {
-      screens: {
-        xs: '300px',
-        sm: '768px',
-        md: '920px',
-        lg: '1330px'
-      },
-      fontFamily: {
-        'poppins': ['Poppins', 'sans-serif']
-      },
-      extend: {
-        colors: {
-          main: {
-            100: '#0f0f0f',
-            200: '#080808'
-          },
-          fontFamily: {
-            'poppins': ['Poppins', 'sans-serif']
-          },
-          secondary: {
-            100: '#F5EFDF'
-          },
-          accent: {
-            100: '#d18100'
-          }
+  content: [
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
+  theme: {
+    screens: {
+      xs: '300px',
+      sm: '768px',
+      md: '920px',
+      lg: '1330px'
+    },
+    fontFamily: {
+      'baumans': ['Baumans', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        main: {
+          100: '#0f0f0f',
+          200: '#080808'
         },
+        fontFamily: {
+          'baumans': ['Baumans', 'sans-serif']
+        },
+        secondary: {
+          100: '#F5EFDF'
+        },
+        accent: {
+          100: '#d18100'
+        },
+        'accent-red': {
+          100: '#7d261e'
+        },
+        'text-yellow': '#e6dcb8'
       },
-    }
-  };
+      borderRadius: {
+        '8xl': '4rem',
+        '9xl': '5rem'
+      }
+    },
+  }
+};

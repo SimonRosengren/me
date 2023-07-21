e0ca9b5c7bb3a53e6808c03c894066a69a81d29d
Simon <simon@tengai.io>
2023-07-21 23:39:00 +0200

    Bauhaus lg

--- pages/index.vue
+++ pages/index.vue
@@ -9,62 +9,98 @@ import { MagicScene } from '~~/.nuxt/components';
             <h2 class="transition ease-in-out text-4xl lg:text-6xl text-main-100 hover:text-secondary-100 font-bold">My name is Simon
                 and I'm a developer</h2>
         </div>
-        <section class="bg-main-100 h-screen sticky top-0">
-            <div class="grid grid-cols-3 lg:grid-cols-11 gap-1 p-3 grid-flow-dense">
-                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm rounded-br-8xl rounded-tl-8xl  flex justify-center items-center">
+        <div class="bg-main-100 h-screen sticky top-0 lg:flex lg:items-center">
+            <div class="grid grid-cols-3 lg:grid-cols-9 gap-1 lg:gap-4 p-3 grid-flow-dense lg:px-32">
+                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm lg:text-lg rounded-br-8xl rounded-tl-8xl  flex justify-center items-center">
                     I code because I love coding.
                 </div>
-                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm flex justify-center items-center">
+                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm lg:text-lg flex justify-center items-center">
                     I learn new things because I love learning
                 </div>
-                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm flex justify-center items-center rounded-b-8xl rounded-tr-8xl">
+                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm lg:text-lg flex justify-center items-center rounded-tr-8xl">
                     
                 </div>
-                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm flex justify-center items-center">
+                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm lg:text-lg flex justify-center items-center">
                     
                 </div>
-                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm flex justify-center items-center">
+                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm lg:text-lg flex justify-center items-center">
                     Avensia
                 </div>
-                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm flex justify-center items-center">
+                <div class="text-text-yellow aspect-square p-2 text-sm lg:text-lg flex justify-center items-center">
                     
                 </div>
-                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm flex justify-center items-center rounded-tl-8xl rounded-br-8xl">
+                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm lg:text-lg flex justify-center items-center rounded-tl-8xl rounded-br-8xl">
                     Bonnier
                 </div>
-                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm flex justify-center items-center">
+                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm lg:text-lg flex justify-center items-center">
                     
                 </div>
-                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm flex justify-center items-center rounded-tr-8xl">
+                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm lg:text-lg flex justify-center items-center rounded-tr-8xl">
                     
                 </div>
-                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm flex justify-center items-center">
+                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm lg:text-lg flex justify-center items-center">
                     
                 </div>
-                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm flex justify-center items-center">
+                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm lg:text-lg flex justify-center items-center">
                     Dahl
                 </div>
-                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm flex justify-center items-center rounded-br-8xl">
+                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm lg:text-lg flex justify-center items-center rounded-br-8xl">
                     Tengai
                 </div>
-                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm flex justify-center items-center rounded-full">
+                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm lg:text-lg flex justify-center items-center rounded-full">
                     
                 </div>
-                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm flex justify-center items-center rounded-full">
+                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm lg:text-lg flex justify-center items-center rounded-full">
                     
                 </div>
-                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm flex justify-center items-center">
+                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm lg:text-lg flex justify-center items-center">
+                    
+                </div>
+                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm lg:text-lg hidden justify-center items-center lg:flex">
+                    
+                </div>
+                <div class="text-text-yellow aspect-square p-2 text-sm lg:text-lg hidden justify-center items-center lg:flex">
+                    
+                </div>
+                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm lg:text-lg hidden justify-center items-center lg:flex">
+                    
+                </div>
+                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm lg:text-lg hidden justify-center items-center lg:flex rounded-tl-8xl rounded-br-8xl">
+                    
+                </div>
+                <div class=" text-text-yellow aspect-square p-2 text-sm lg:text-lg hidden justify-center items-center lg:flex">
+                    
+                </div>
+                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm lg:text-lg hidden justify-center items-center lg:flex rounded-br-8xl">
+                    
+                </div>
+                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm lg:text-lg hidden justify-center items-center lg:flex">
+                    
+                </div>
+                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm lg:text-lg hidden justify-center items-center lg:flex rounded-full">
+                    
+                </div>
+                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm lg:text-lg hidden justify-center items-center lg:flex">
+                    
+                </div>
+                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm lg:text-lg hidden justify-center items-center lg:flex rounded-tl-8xl rounded-br-8xl">
+                    
+                </div>
+                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm lg:text-lg hidden justify-center items-center lg:flex">
+                    
+                </div>
+                <div class="bg-accent-red-100 text-text-yellow aspect-square p-2 text-sm lg:text-lg hidden justify-center items-center lg:flex">
                     
                 </div>
                 <div class="col-span-full py-16 flex justify-end">
-                    <div class="text-text-yellow text-lg border-b hover:bg-text-yellow hover:bg-opacity-5 cursor-pointer border-text-yellow p-4">
+                    <div class="text-text-yellow text-lg lg:text-2xl border-b hover:bg-text-yellow hover:bg-opacity-5 cursor-pointer border-text-yellow p-4" @click="openMail">
                         Contact me
                     </div>
                 </div>
             </div>
             <!-- <div class="lg:w-1/3">I build things because I love building things. I learn new things because I love learning new things. Curiosity is my super power</div>
             <div class="border border-secondary-100 rounded-full p-4 text-center text-xl lg:text-2xl max-w-[300px] mt-24 cursor-pointer transition-colors hover:bg-secondary-100 hover:bg-opacity-10" @click="openMail">Contact me -></div> -->
-        </section>
+            </div>
     </div>
 </template>
 

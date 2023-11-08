443c8fa4a2a07e5e8ff1fdb7482e1338d42fb823
Simon <simon@tengai.io>
2023-11-08 23:07:24 +0100

    Some text

--- pages/index.vue
+++ pages/index.vue
@@ -7,11 +7,11 @@
                 <div class="flex flex-col gap-5">
                     <div class="border-b lg:border-r font-bold pb-3 pr-64">
                         <div class=" max-w-xs lg:max-w-6xl">
-                            <h2 class="lg:text-7xl lg:text-9xl leading-[.9] text-7xl text-neutral-100 font-bold">Hello.</h2>
-                            <h2 class="lg:text-7xl lg:text-9xl leading-[.9] text-7xl text-neutral-100 font-bold">I am Simon</h2>
+                            <h2 class="lg:text-9xl leading-[.9] text-7xl text-neutral-100 font-bold">Hello.</h2>
+                            <h2 class="lg:text-9xl leading-[.9] text-7xl text-neutral-100 font-bold">I am Simon</h2>
                         </div>
                     </div>
-                    <Button @click="navigateTo({ path: '/', hash: '#did' })">
+                    <Button @click="navigateTo({ path: '/', hash: '#me' })">
                         <template #body>Explore</template>
                     </Button>
                 </div>
@@ -19,107 +19,38 @@
             </div>
         </section>
         <section id="me">
-            <div class="bg-neutral-300 h-screen flex items-center lg:px-32 relative">
-                <div class="w-full flex flex-col lg:flex-row gap-14 items-center">
-                    <div class="">
+            <div class="bg-neutral-300 h-screen flex items-center lg:px-96 relative">
+                <div class="w-full flex flex-col lg:flex-row">
+                    <div class="border-r pr-10">
                         <h2 class=" text-7xl lg:text-9xl font-bold text-neutral-100">Who</h2>
                         <h3 class=" text-7xl lg:text-9xl font-bold text-neutral-100 -mt-4">am I</h3>
                     </div>
-                    <div class="p-4 rounded-tr-9xl border text-neutral-100 border-red-200">
-                        Mollit sit et nulla officia. Mollit consequat minim amet enim ullamco magna commodo consectetur magna laboris sit. Ea ipsum elit irure veniam ipsum cupidatat dolor qui sunt labore.
+                    <div class="text-neutral-100 text-sm md:text-lg lg:text-2xl pl-10">
+                        I'm a full-stack developer who thrives on exploring the ever-evolving tech landscape. From designing and crafting seamless user experiences to diving deep into the backend, I find joy in every aspect of the development process. With a passion for innovation and a love for continuous learning, I'm constantly seeking new challenges to expand my skillset. Whether it's building elegant front-end interfaces or architecting robust server-side solutions, I'm up for the task. Let's build something cool together.
                     </div>
-                    <!-- <div class="grid grid-cols-3 lg:grid-cols-8 gap-1 lg:gap-4 p-3 grid-flow-dense w-full">
-                        <CardClickable class=" aspect-square col-span-2 text-sm lg:text-lg rounded-br-8xl rounded-tl-8xl  flex justify-center items-center">
-                            Avensia
-                        </CardClickable>
-                        <CardClickable class=" aspect-square col-span-2 text-sm lg:text-lg flex justify-center items-center">
-                            Bonnier
-                        </CardClickable>  
-                        <CardClickable
-                            class=" aspect-square col-span-2 text-sm lg:text-lg flex justify-center items-center rounded-tr-8xl">
-                            Dahl
-                        </CardClickable>
-                        <CardClickable
-                            class=" aspect-square text-sm col-span-2 lg:text-lg flex justify-center items-center rounded-br-8xl">
-                            Tengai
-                        </CardClickable>
-                        <CardClickable class=" aspect-square text-sm lg:text-lg flex justify-center items-center rounded-br-8xl" />
-                        <CardClickable class=" aspect-square text-sm lg:text-lg flex justify-center items-center rounded-bl-8xl" />
-                        <CardClickable class=" aspect-square text-sm lg:text-lg flex justify-center items-center rounded-tr-8xl" />
-                        <CardClickable class=" aspect-square text-sm lg:text-lg flex justify-center items-center rounded-tl-8xl " />
-                        <CardClickable class=" aspect-square text-sm lg:text-lg flex justify-center items-center rounded-br-8xl" />
-                        <CardClickable class=" aspect-square text-sm lg:text-lg flex justify-center items-center rounded-br-8xl" />
-                        <CardClickable class=" aspect-square text-sm lg:text-lg flex justify-center items-center rounded-br-8xl" />
-                        <CardClickable class=" aspect-square text-sm lg:text-lg flex justify-center items-center rounded-br-8xl" />
-                    </div> -->
+                </div>
+            </div>
+        </section>
+        <section id="do">
+            <div class="bg-neutral-300 h-screen flex items-center lg:px-96 relative">
+                <div class="w-full flex flex-col lg:flex-row gap-14 items-center">
+                    <div class="border-r pr-10">
+                        <h2 class=" text-7xl lg:text-9xl font-bold text-neutral-100">What</h2>
+                        <h3 class=" text-7xl lg:text-9xl font-bold text-neutral-100 -mt-4">I do</h3>
+                    </div>
+                    <div class="text-neutral-100 text-sm md:text-lg lg:text-2xl pl-10">
+                        I work as a Full-Stack Developer at a startup, contributing in DevOps, Backend, aswell as Frontend development. Additionally, I have the role of Lead Frontend developer, focusing on user interface and experience. This role involves managing deployment, optimizing automation, designing server-side solutions, and pushing the boundaries of web applications (especially on Safari...). It combines technical expertise with leadership to support the startup's innovative solutions.
+                    </div>
                 </div>
             </div>
         </section>
         <section id="did">
-            <div class="bg-neutral-300 h-screen flex items-center lg:px-32 relative">
+            <div class="bg-neutral-300 h-screen flex items-center lg:px-96 relative">
                 <div class="w-full flex flex-col lg:flex-row gap-14 items-center">
                     <div class="">
                         <h2 class=" text-7xl lg:text-9xl font-bold text-neutral-100">What</h2>
                         <h3 class=" text-7xl lg:text-9xl font-bold text-neutral-100 -mt-4">I did</h3>
                     </div>
-                    <!-- <div class="grid grid-cols-3 lg:grid-cols-8 gap-1 lg:gap-4 p-3 grid-flow-dense w-full">
-                        <CardClickable class=" aspect-square col-span-2 text-sm lg:text-lg rounded-br-8xl rounded-tl-8xl  flex justify-center items-center">
-                            Avensia
-                        </CardClickable>
-                        <CardClickable class=" aspect-square col-span-2 text-sm lg:text-lg flex justify-center items-center">
-                            Bonnier
-                        </CardClickable>  
-                        <CardClickable
-                            class=" aspect-square col-span-2 text-sm lg:text-lg flex justify-center items-center rounded-tr-8xl">
-                            Dahl
-                        </CardClickable>
-                        <CardClickable
-                            class=" aspect-square text-sm col-span-2 lg:text-lg flex justify-center items-center rounded-br-8xl">
-                            Tengai
-                        </CardClickable>
-                        <CardClickable class=" aspect-square text-sm lg:text-lg flex justify-center items-center rounded-br-8xl" />
-                        <CardClickable class=" aspect-square text-sm lg:text-lg flex justify-center items-center rounded-bl-8xl" />
-                        <CardClickable class=" aspect-square text-sm lg:text-lg flex justify-center items-center rounded-tr-8xl" />
-                        <CardClickable class=" aspect-square text-sm lg:text-lg flex justify-center items-center rounded-tl-8xl " />
-                        <CardClickable class=" aspect-square text-sm lg:text-lg flex justify-center items-center rounded-br-8xl" />
-                        <CardClickable class=" aspect-square text-sm lg:text-lg flex justify-center items-center rounded-br-8xl" />
-                        <CardClickable class=" aspect-square text-sm lg:text-lg flex justify-center items-center rounded-br-8xl" />
-                        <CardClickable class=" aspect-square text-sm lg:text-lg flex justify-center items-center rounded-br-8xl" />
-                    </div> -->
-                </div>
-            </div>
-        </section>
-        <section id="do">
-            <div class="bg-neutral-300 h-screen flex items-center lg:px-32 relative">
-                <div class="w-full flex flex-col lg:flex-row gap-14 items-center">
-                    <div class="">
-                        <h2 class=" text-7xl lg:text-9xl font-bold text-neutral-100">What</h2>
-                        <h3 class=" text-7xl lg:text-9xl font-bold text-neutral-100 -mt-4">I do</h3>
-                    </div>
-                    <!-- <div class="grid grid-cols-3 lg:grid-cols-8 gap-1 lg:gap-4 p-3 grid-flow-dense w-full">
-                        <CardClickable class=" aspect-square col-span-2 text-sm lg:text-lg rounded-br-8xl rounded-tl-8xl  flex justify-center items-center">
-                            Avensia
-                        </CardClickable>
-                        <CardClickable class=" aspect-square col-span-2 text-sm lg:text-lg flex justify-center items-center">
-                            Bonnier
-                        </CardClickable>  
-                        <CardClickable
-                            class=" aspect-square col-span-2 text-sm lg:text-lg flex justify-center items-center rounded-tr-8xl">
-                            Dahl
-                        </CardClickable>
-                        <CardClickable
-                            class=" aspect-square text-sm col-span-2 lg:text-lg flex justify-center items-center rounded-br-8xl">
-                            Tengai
-                        </CardClickable>
-                        <CardClickable class=" aspect-square text-sm lg:text-lg flex justify-center items-center rounded-br-8xl" />
-                        <CardClickable class=" aspect-square text-sm lg:text-lg flex justify-center items-center rounded-bl-8xl" />
-                        <CardClickable class=" aspect-square text-sm lg:text-lg flex justify-center items-center rounded-tr-8xl" />
-                        <CardClickable class=" aspect-square text-sm lg:text-lg flex justify-center items-center rounded-tl-8xl " />
-                        <CardClickable class=" aspect-square text-sm lg:text-lg flex justify-center items-center rounded-br-8xl" />
-                        <CardClickable class=" aspect-square text-sm lg:text-lg flex justify-center items-center rounded-br-8xl" />
-                        <CardClickable class=" aspect-square text-sm lg:text-lg flex justify-center items-center rounded-br-8xl" />
-                        <CardClickable class=" aspect-square text-sm lg:text-lg flex justify-center items-center rounded-br-8xl" />
-                    </div> -->
                 </div>
             </div>
         </section>
